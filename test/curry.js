b9efe2f5470a460643196bbc45badb6f6fa5fa12
ajlopez <webmaster@ajlopez.com>
2014-06-23 14:18:03 -0300

    Partial apply with skip, first test

--- test/curry.js
+++ test/curry.js
@@ -29,3 +29,14 @@ exports['call with arguments one by one'] = function (test) {
     test.equal(add(1)(2), 3);
 };
 
+exports['call functions with three arguments one by one'] = function (test) {
+    var fn = _.curry(function (x, y, z) { return x + y * z; });
+    test.equal(fn(1)(2)(3), 7);
+};
+
+exports['call functions with three arguments applying skip'] = function (test) {
+    var fn = _.curry(function (x, y, z) { return x + y * z; });
+    var fn2 = fn(_,2);
+    test.equal(fn2(1,3), 7);
+};
+
